db6a0d4fc8dafd2f67b006bf771e1fec61c4a49d
WalterHeitor <waterheitorfo@gmail.com>
2024-02-13 23:31:31 -0300

    feat:intit

---
+++ src/main/java/br/com/softwalter/SpringbootLocalstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.softwalter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLocalstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softwalter/SpringbootLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softwalter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLocalstackApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
